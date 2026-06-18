c63c174510aed5672bda18c7082db4e0c490f30e
koiraladharana <koiraladharana21@gmail.com>
2026-06-18 06:40:17 +0000

    Set up Express server and connect to MongoDB Atlas

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import connectDB from './src/config/db.js';
+import app from './src/app.js';
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
