de43f292b4dba58b634dbce2593f140ff83ad0a4
koiraladharana <koiraladharana21@gmail.com>
2026-06-21 09:42:19 +0000

    fix authMiddleware import extension

--- server/src/routes/favoriteRoutes.js
+++ server/src/routes/favoriteRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getFavorites, addFavorite, removeFavorite} from '../controllers/favoriteController.js';
-import protect from '../middleware/authMiddleware';
+import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
